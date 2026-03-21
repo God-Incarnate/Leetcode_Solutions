7d8fea9462541837d38f830658ac32f4a09bf88b
God-Incarnate <prashant11111999@gmail.com>
2026-03-22 00:59:01 +0530

    Group Anagrams solution

--- src/JAVA/MEDIUM/GroupAnagrams.java
+++ src/JAVA/MEDIUM/GroupAnagrams.java
@@ -1,5 +1,23 @@
 package JAVA.MEDIUM;
 
+import java.util.*;
+
 public class GroupAnagrams {
     //https://leetcode.com/problems/group-anagrams
+
+    //Sorting as keys
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
 }
