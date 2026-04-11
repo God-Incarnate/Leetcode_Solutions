c2780249eb7e3e7a891a7d84091120f988cfb4d0
God-Incarnate <prashant11111999@gmail.com>
2026-04-11 12:23:21 +0530

    Diameter of a binary tree

--- src/JAVA/EASY/DiameterOfBinaryTree.java
+++ src/JAVA/EASY/DiameterOfBinaryTree.java
@@ -16,5 +16,20 @@ public class DiameterOfBinaryTree {
           }
       }
 
+    class Solution {
+        int max=0;
+        public int diameterOfBinaryTree(TreeNode root) {
+            heightChecker(root);
+            return max;
+        }
+        private int heightChecker(TreeNode root){
+            if (root==null) return 0;
+            int heightOfLeft=heightChecker(root.left);
+            int heightOfRight=heightChecker(root.right);
+            max=Math.max(max,heightOfLeft+heightOfRight);
+            return 1+Math.max(heightOfLeft,heightOfRight);
 
+        }
+
+    }
 }
