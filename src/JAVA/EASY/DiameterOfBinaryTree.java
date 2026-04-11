8e12639a9f4b2f4c516f67fc727cae2f39274605
God-Incarnate <prashant11111999@gmail.com>
2026-04-11 12:23:07 +0530

    Diameter of a binary tree

--- src/JAVA/EASY/DiameterOfBinaryTree.java
+++ src/JAVA/EASY/DiameterOfBinaryTree.java
@@ -3,5 +3,18 @@ package JAVA.EASY;
 public class DiameterOfBinaryTree {
     //https://leetcode.com/problems/diameter-of-binary-tree
 
-     
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+
 }
