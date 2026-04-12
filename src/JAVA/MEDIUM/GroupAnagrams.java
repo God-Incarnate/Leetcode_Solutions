efd0a5c093005b7f147894ce18f79a1bf31da1bd
God-Incarnate <prashant11111999@gmail.com>
2026-04-12 17:59:19 +0530

    Invert binary tree

--- src/JAVA/MEDIUM/GroupAnagrams.java
+++ src/JAVA/MEDIUM/GroupAnagrams.java
@@ -6,18 +6,17 @@ public class GroupAnagrams {
     //https://leetcode.com/problems/group-anagrams
 
     //Sorting as keys
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
-
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            HashMap<String,List<String>> res=new HashMap<>();
+            for(String s:strs){
+                char[] charArrayOfS=s.toCharArray();
+                Arrays.sort(charArrayOfS);
+                String key=new String(charArrayOfS);
+                res.putIfAbsent(key,new ArrayList<>());
+                res.get(key).add(s);
+            }
+            return new ArrayList<>(res.values());
         }
-
-        return new ArrayList<>(map.values());
     }
 }
