d1c14fa2285050f629929646b567c191cb7006a6
God-Incarnate <prashant11111999@gmail.com>
2026-04-12 18:01:14 +0530

    kth larggest element

--- src/JAVA/EASY/KthLargestElement.java
+++ src/JAVA/EASY/KthLargestElement.java
@@ -16,7 +16,15 @@ public class KthLargestElement {
             }
         }
 
-
+        public int add(int val) {
+            if(minHeap.size()<k){
+                minHeap.offer(val);
+            } else if(val>minHeap.peek()){
+                minHeap.poll();
+                minHeap.offer(val);
+            }
+            return minHeap.peek();
+        }
 
     }
 
