e1fb56c00beaf7e45ccba6da2150ff21ad90d93f
God-Incarnate <prashant11111999@gmail.com>
2026-04-13 09:31:22 +0530

    Last Stone weight

--- src/JAVA/EASY/LastStoneWeight.java
+++ src/JAVA/EASY/LastStoneWeight.java
@@ -5,5 +5,18 @@ import java.util.PriorityQueue;
 
 public class LastStoneWeight {
     //https://leetcode.com/problems/last-stone-weight
-    
+    class Solution {
+        public int lastStoneWeight(int[] stones) {
+            PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+            for(int nums:stones){
+                maxHeap.offer(nums);
+            }
+            while(maxHeap.size()!=1){
+                int p=maxHeap.poll();
+                int q=maxHeap.poll();
+                maxHeap.offer(p-q);
+            }
+            return maxHeap.peek();
+        }
+    }
 }
