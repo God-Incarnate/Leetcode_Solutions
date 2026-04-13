a65d33a37930e4f309b10e1703044798e4366a91
God-Incarnate <prashant11111999@gmail.com>
2026-04-13 09:32:29 +0530

    Linked List cycle

--- src/JAVA/EASY/LinkedListCycle.java
+++ src/JAVA/EASY/LinkedListCycle.java
@@ -2,5 +2,14 @@ package JAVA.EASY;
 
 public class LinkedListCycle {
     //https://leetcode.com/problems/linked-list-cycle
+      class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
 
 }
