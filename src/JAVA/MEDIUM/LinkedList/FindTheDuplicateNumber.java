881d7dcbbf62041d459e0aa9e9d4bf247b829038
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:03:20 +0530

    Find the duplicate number

--- src/JAVA/MEDIUM/LinkedList/FindTheDuplicateNumber.java
+++ src/JAVA/MEDIUM/LinkedList/FindTheDuplicateNumber.java
@@ -2,5 +2,16 @@ package JAVA.MEDIUM.LinkedList;
 
 public class FindTheDuplicateNumber {
     //https://leetcode.com/problems/find-the-duplicate-number
-
+    class Solution {
+        public int findDuplicate(int[] nums) {
+            for (int num : nums) {
+                int idx = Math.abs(num) - 1;
+                if (nums[idx] < 0) {
+                    return Math.abs(num);
+                }
+                nums[idx] *= -1;
+            }
+            return -1;
+        }
+    }
 }
