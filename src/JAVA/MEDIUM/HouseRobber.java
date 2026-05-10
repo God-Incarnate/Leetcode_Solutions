4e7b97a447040e96a80275aa3358f12403383fb5
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:44:45 +0530

    house robber

--- src/JAVA/MEDIUM/HouseRobber.java
+++ src/JAVA/MEDIUM/HouseRobber.java
@@ -2,5 +2,18 @@ package JAVA.MEDIUM;
 
 public class HouseRobber {
     //https://leetcode.com/problems/house-robber
+    class Solution {
+        public int rob(int[] nums) {
+            if(nums.length==1) return nums[0];
+            int[] dp=new int[nums.length];
+            dp[0]=nums[0];
+            dp[1]=Math.max(nums[0],nums[1]);
+            for(int i=2;i<nums.length;i++){
+                dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            }
+            return dp[nums.length-1];
+        }
+
+    }
 
 }
