e7e5c2185963218301b7228dd4d3b08c92a367c5
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:20:17 +0530

    KthLargestELEM

--- src/JAVA/EASY/HeapOrPriorityQueue/KthLargestElement.java
+++ src/JAVA/EASY/HeapOrPriorityQueue/KthLargestElement.java
@@ -4,7 +4,29 @@ import java.util.PriorityQueue;
 
 public class KthLargestElement {
     //https://leetcode.com/problems/kth-largest-element-in-a-stream
+    class KthLargest {
+        int k;
+        PriorityQueue<Integer> minHeap;
 
+        public KthLargest(int k, int[] nums) {
+            this.k=k;
+            this.minHeap=new PriorityQueue<>();
+            for (int i=0;i<nums.length;i++){
+                add(nums[i]);
+            }
+        }
+
+        public int add(int val) {
+            if(minHeap.size()<k){
+                minHeap.offer(val);
+            } else if(val>minHeap.peek()){
+                minHeap.poll();
+                minHeap.offer(val);
+            }
+            return minHeap.peek();
+        }
+
+    }
 
 /**
  * Your KthLargest object will be instantiated and called as such:
