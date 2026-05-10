b0bdf2bdaa9707af9813fb00b0b3a183ccad9c8f
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:54:49 +0530

    max are of island

--- src/JAVA/MEDIUM/MaxAreaOfIsland.java
+++ src/JAVA/MEDIUM/MaxAreaOfIsland.java
@@ -2,5 +2,28 @@ package JAVA.MEDIUM;
 
 public class MaxAreaOfIsland {
     //https://leetcode.com/problems/max-area-of-island
+    class Solution {
+        public int maxAreaOfIsland(int[][] grid) {
+            int maxAr = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    maxAr = Math.max(maxAr, arCount(grid, i, j));
+                }
+            }
+            return maxAr;
+        }
+
+        private int arCount(int[][] grid, int i, int j) {
+            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
+                return 0;
+            grid[i][j] = 0;
+            return 1
+                    + arCount(grid, i + 1, j)
+                    + arCount(grid, i - 1, j)
+                    + arCount(grid, i, j + 1)
+                    + arCount(grid, i, j - 1);
+
+        }
+    }
 
 }
