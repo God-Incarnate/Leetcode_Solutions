af068adc9a41092e51596b6a6c7d124abe354bd6
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:25:51 +0530

    ReverseLinkedList

--- src/JAVA/EASY/LinkedList/ReverseLinkedList.java
+++ src/JAVA/EASY/LinkedList/ReverseLinkedList.java
@@ -2,5 +2,12 @@ package JAVA.EASY.LinkedList;
 
 public class ReverseLinkedList {
     //https://leetcode.com/problems/reverse-linked-list
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
 
 }
