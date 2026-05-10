38d3c457468bfa84cdd59926d17a2e53d4708b89
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:35:08 +0530

    InvertBT

--- src/JAVA/EASY/Trees/InvertBinaryTree.java
+++ src/JAVA/EASY/Trees/InvertBinaryTree.java
@@ -3,5 +3,18 @@ package JAVA.EASY.Trees;
 public class InvertBinaryTree {
     //https://leetcode.com/problems/invert-binary-tree
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
 }
