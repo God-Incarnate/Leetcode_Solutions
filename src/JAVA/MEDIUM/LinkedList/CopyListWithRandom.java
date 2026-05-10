617e627fb7bbbd8a064a4438eb07e857248c8a04
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:01:50 +0530

    copy list with random pointer

---
+++ src/JAVA/MEDIUM/LinkedList/CopyListWithRandom.java
@@ -0,0 +1,9 @@
+package JAVA.MEDIUM.LinkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CopyListWithRandom {
+    //https://leetcode.com/problems/copy-list-with-random-pointer
+
+}
