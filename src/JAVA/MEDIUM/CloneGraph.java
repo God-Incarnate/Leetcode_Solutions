c555a449f206a91dc1c356773e4c4d6fff308992
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:31:55 +0530

    clone graph

--- src/JAVA/MEDIUM/CloneGraph.java
+++ src/JAVA/MEDIUM/CloneGraph.java
@@ -44,5 +44,34 @@ class Node {
             return clone;
         }
     }
+    /*
+    import java.util.*;
 
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        // init
+        queue.offer(node);
+        map.put(node, new Node(node.val));
+
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+
+            for (Node nei : curr.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new Node(nei.val));
+                    queue.offer(nei);
+                }
+                map.get(curr).neighbors.add(map.get(nei));
+            }
+        }
+
+        return map.get(node);
+    }
+}
+     */
 }
