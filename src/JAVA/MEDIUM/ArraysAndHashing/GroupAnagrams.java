3d601ab27503cb8b687052a9895bdd1d9556e09b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:44:42 +0530

    group anagrams

--- src/JAVA/MEDIUM/ArraysAndHashing/GroupAnagrams.java
+++ src/JAVA/MEDIUM/ArraysAndHashing/GroupAnagrams.java
@@ -6,5 +6,17 @@ public class GroupAnagrams {
     //https://leetcode.com/problems/group-anagrams
 
     //Sorting as keys
-
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            HashMap<String,List<String>> res=new HashMap<>();
+            for(String s:strs){
+                char[] charArrayOfS=s.toCharArray();
+                Arrays.sort(charArrayOfS);
+                String key=new String(charArrayOfS);
+                res.putIfAbsent(key,new ArrayList<>());
+                res.get(key).add(s);
+            }
+            return new ArrayList<>(res.values());
+        }
+    }
 }
