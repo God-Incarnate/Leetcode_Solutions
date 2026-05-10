b4fb9b2f21921577c8f0f0388e1a7346f631511a
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:22:39 +0530

    LinkedListCycle

--- src/JAVA/EASY/LinkedList/LinkedListCycle.java
+++ src/JAVA/EASY/LinkedList/LinkedListCycle.java
@@ -11,5 +11,19 @@ public class LinkedListCycle {
           }
       }
 
-
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if(head==null || head.next==null) return false;
+            ListNode oneStep=head;
+            ListNode twoStep=head.next;
+            while(twoStep!=null && twoStep.next!=null){
+                if(oneStep==twoStep){
+                    return true;
+                }
+                oneStep=oneStep.next;
+                twoStep=twoStep.next.next;
+            }
+            return false;
+        }
+    }
 }
