c5f400e1acf00f9642e17d57692ef0eef94d21a5
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:17:16 +0530

    evaluate reverse polish notation

--- src/JAVA/MEDIUM/Stack/EvaluateReversePolishNotation.java
+++ src/JAVA/MEDIUM/Stack/EvaluateReversePolishNotation.java
@@ -4,5 +4,27 @@ import java.util.Stack;
 
 public class EvaluateReversePolishNotation {
     //https://leetcode.com/problems/evaluate-reverse-polish-notation
+    class Solution {
+        public int evalRPN(String[] tokens) {
+            Stack<Integer> calculator = new Stack<>();
+            for (String token : tokens) {
+                if (!"*-+/".contains(token)) {
+                    calculator.push(Integer.parseInt(token));
+                    continue;
+                }
+                int b = calculator.pop();
+                int a = calculator.pop();
+                if (token.equals("*"))
+                    calculator.push(a * b);
+                else if (token.equals("+"))
+                    calculator.push(a + b);
+                else if (token.equals("-"))
+                    calculator.push(a - b);
+                else
+                    calculator.push(a / b);
 
+            }
+            return calculator.pop();
+        }
+    }
 }
