1187642c0835a38132d24c610a212a45ad645b60
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:00:33 +0530

    time based key value store

--- src/JAVA/MEDIUM/BinarySearch/TimeBasedKeyValueStore.java
+++ src/JAVA/MEDIUM/BinarySearch/TimeBasedKeyValueStore.java
@@ -7,6 +7,49 @@ import java.util.Map;
 
 public class TimeBasedKeyValueStore {
     //https://leetcode.com/problems/time-based-key-value-store
+    class TimeMap {
+        Map<String, List<Pair>> map;
+
+        public TimeMap() {
+            map = new HashMap<>();
+        }
+
+        public void set(String key, String value, int timestamp) {
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(new Pair(timestamp, value));
+        }
+
+        public String get(String key, int timestamp) {
+            if (!map.containsKey(key)) return "";
+
+            List<Pair> list = map.get(key);
+
+            int l = 0, r = list.size() - 1;
+            String res = "";
+
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (list.get(mid).time <= timestamp) {
+                    res = list.get(mid).val;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            return res;
+        }
+
+        static class Pair {
+            int time;
+            String val;
+            Pair(int t, String v) {
+                time = t;
+                val = v;
+            }
+        }
+    }
+
 
 /**
  * Your TimeMap object will be instantiated and called as such:
