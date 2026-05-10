251fcbf7f5bee1a560300ca0019e5f0cc126924b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:26:58 +0530

    lowest common ancestor

--- src/JAVA/MEDIUM/Trees/LowestCommonAncestorBST.java
+++ src/JAVA/MEDIUM/Trees/LowestCommonAncestorBST.java
@@ -11,5 +11,15 @@ public class LowestCommonAncestorBST {
       }
 
 
-
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            TreeNode current=root;
+            while(current!=null){
+                if(p.val<current.val && q.val<current.val) current=current.left;
+                else if (p.val>current.val && q.val>current.val) current=current.right;
+                else return current;
+            }
+            return root;
+        }
+    }
 }
