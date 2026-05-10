1daab3000b2cff46d0cb956a23f0edafa6c07050
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:26:06 +0530

    ReverseLinkedList

--- src/JAVA/EASY/LinkedList/ReverseLinkedList.java
+++ src/JAVA/EASY/LinkedList/ReverseLinkedList.java
@@ -9,5 +9,17 @@ public class ReverseLinkedList {
           ListNode(int val) { this.val = val; }
           ListNode(int val, ListNode next) { this.val = val; this.next = next; }
       }
-
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode current=head;
+            ListNode prev=null;
+            while (current!=null){
+                ListNode nextNode=current.next;
+                current.next=prev;
+                prev=current;
+                current=nextNode;
+            }
+            return prev;
+        }
+    }
 }
