02909f8faf2f2a9e9a2264354a1ea9609062714e
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:33:17 +0530

    container with most water

--- src/JAVA/MEDIUM/TwoPointers/ContainerWithMostWater.java
+++ src/JAVA/MEDIUM/TwoPointers/ContainerWithMostWater.java
@@ -2,5 +2,20 @@ package JAVA.MEDIUM.TwoPointers;
 
 public class ContainerWithMostWater {
     //https://leetcode.com/problems/container-with-most-water
-
+    class Solution {
+        public int maxArea(int[] height) {
+            int left=0;
+            int right=height.length-1;
+            int maxAr=0;
+            while(left<right){
+                maxAr=Math.max(maxAr,(Math.min(height[left],height[right])*(right-left)));
+                if(height[left]<height[right]){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+            return maxAr;
+        }
+    }
 }
