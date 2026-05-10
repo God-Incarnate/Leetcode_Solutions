770c8bf90b2b05d861fffcb1bb1728c02723e059
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:39:25 +0530

    subtree of another tree

--- src/JAVA/EASY/Trees/SubtreeOfAnotherTree.java
+++ src/JAVA/EASY/Trees/SubtreeOfAnotherTree.java
@@ -3,5 +3,18 @@ package JAVA.EASY.Trees;
 public class SubtreeOfAnotherTree {
     //https://leetcode.com/problems/subtree-of-another-tree
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
 }
