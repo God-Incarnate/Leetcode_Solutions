44fd15a5d2068f1a1ca6d9949e9ec0ef67552e2c
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:26:59 +0530

    ClimbingStairs

--- src/JAVA/EASY/OneDDP/ClimbingStairs.java
+++ src/JAVA/EASY/OneDDP/ClimbingStairs.java
@@ -2,5 +2,17 @@ package JAVA.EASY.OneDDP;
 
 public class ClimbingStairs {
     //https://leetcode.com/problems/climbing-stairs
+    class Solution {
+        public int climbStairs(int n) {
+            if (n<=2) return n;
+            int[] stepWays=new int[n+1];
+            stepWays[1]=1;
+            stepWays[2]=2;
 
+            for(int i=3;i<=n;i++){
+                stepWays[i]=stepWays[i-1]+stepWays[i-2];
+            }
+            return stepWays[n];
+        }
+    }
 }
