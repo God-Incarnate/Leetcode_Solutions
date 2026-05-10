624f956b0473574d13695e6b0b30019a56f653b8
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:00:03 +0530

    time based key value store

---
+++ src/JAVA/MEDIUM/BinarySearch/TimeBasedKeyValueStore.java
@@ -0,0 +1,17 @@
+package JAVA.MEDIUM.BinarySearch;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TimeBasedKeyValueStore {
+    //https://leetcode.com/problems/time-based-key-value-store
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+}
