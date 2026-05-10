20be10942127e4ac87a58f0c8336505b1dce0700
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:32:11 +0530

    BalancedBT

--- src/JAVA/EASY/Trees/BalancedBinaryTree.java
+++ src/JAVA/EASY/Trees/BalancedBinaryTree.java
@@ -2,5 +2,17 @@ package JAVA.EASY.Trees;
 
 public class BalancedBinaryTree {
     //https://leetcode.com/problems/balanced-binary-tree
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
 
 }
