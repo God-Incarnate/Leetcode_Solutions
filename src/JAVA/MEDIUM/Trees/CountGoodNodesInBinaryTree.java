fbd17eaf7c94dac874e55d5e557721ed1038980f
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:22:45 +0530

    counting good nodes in binary tree

--- src/JAVA/MEDIUM/Trees/CountGoodNodesInBinaryTree.java
+++ src/JAVA/MEDIUM/Trees/CountGoodNodesInBinaryTree.java
@@ -3,5 +3,18 @@ package JAVA.MEDIUM.Trees;
 public class CountGoodNodesInBinaryTree {
     //https://leetcode.com/problems/count-good-nodes-in-binary-tree
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
 }
