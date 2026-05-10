a14c36a3e48e2bd009bdb07b1a8d1c019268ff8d
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:07:08 +0530

    Reorder list

---
+++ src/JAVA/MEDIUM/LinkedList/ReorderList.java
@@ -0,0 +1,18 @@
+package JAVA.MEDIUM.LinkedList;
+
+import JAVA.structures.ListNode;
+
+public class ReorderList {
+    //https://leetcode.com/problems/reorder-list
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+}
