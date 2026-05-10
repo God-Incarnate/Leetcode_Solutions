5b58aa171d79040378cdfcd47bf7c093b28066f4
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:25:10 +0530

    level order traversal

--- src/JAVA/MEDIUM/Trees/LevelOrderTraversal.java
+++ src/JAVA/MEDIUM/Trees/LevelOrderTraversal.java
@@ -21,5 +21,27 @@ public class LevelOrderTraversal {
           }
       }
 
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result=new ArrayList<>();
 
+            if(root==null) return result;
+
+            Queue<TreeNode> visit=new LinkedList<>();
+
+            visit.offer(root);
+            while(!visit.isEmpty()){
+                int size=visit.size();
+                List<Integer> layer=new LinkedList<>();
+                for(int i=0;i<size;i++){
+                    TreeNode value=visit.poll();
+                    if(value.left!=null) visit.add(value.left);
+                    if(value.right!=null) visit.add(value.right);
+                    layer.add(value.val);
+                }
+                result.add(layer);
+            }
+            return result;
+        }
+    }
 }
