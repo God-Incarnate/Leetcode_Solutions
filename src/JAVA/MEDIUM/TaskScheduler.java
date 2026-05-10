8ef867f30000cede238e2474db4581372b7f44b9
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 20:04:16 +0530

    task scheduler

--- src/JAVA/MEDIUM/TaskScheduler.java
+++ src/JAVA/MEDIUM/TaskScheduler.java
@@ -2,5 +2,24 @@ package JAVA.MEDIUM;
 
 public class TaskScheduler {
     //https://leetcode.com/problems/task-scheduler
+    class Solution {
+        public int leastInterval(char[] tasks, int n) {
+            int[] freq=new int[26];
+            for(char ch:tasks){
+                freq[ch-'A']++;
+            }
+            int maxV=0;
+            for (int num:freq){
+                maxV=Math.max(maxV,num);
+            }
 
+            int countMax=0;
+            for (int num: freq){
+                if(num==maxV) countMax++;
+            }
+
+            int result=(maxV-1)*(n+1)+countMax;
+            return Math.max(tasks.length,result);
+        }
+    }
 }
