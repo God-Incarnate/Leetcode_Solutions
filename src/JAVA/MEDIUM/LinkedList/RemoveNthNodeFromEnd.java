30921b3e136023390602677dc9b78c61ef6a5f05
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:05:54 +0530

    Remove nth node

---
+++ src/JAVA/MEDIUM/LinkedList/RemoveNthNodeFromEnd.java
@@ -0,0 +1,18 @@
+package JAVA.MEDIUM.LinkedList;
+
+import JAVA.structures.ListNode;
+
+public class RemoveNthNodeFromEnd {
+    //https://leetcode.com/problems/remove-nth-node-from-end-of-list
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+}
