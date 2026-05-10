1a62b48c86388d0e0e045031d27f8c56b24f62f6
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:51:49 +0530

    k largest array

--- src/JAVA/MEDIUM/KthLargestElementInArray.java
+++ src/JAVA/MEDIUM/KthLargestElementInArray.java
@@ -4,5 +4,11 @@ import java.util.Arrays;
 
 public class KthLargestElementInArray {
     //https://leetcode.com/problems/kth-largest-element-in-an-array
-
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            Arrays.sort(nums);
+            int n=nums.length;
+            return nums[n-k];
+        }
+    }
 }
