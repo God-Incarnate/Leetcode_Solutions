865027efaed6feb7a2db4d4e92daf75db0e02d66
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:07:37 +0530

    Reorder list

--- src/JAVA/MEDIUM/LinkedList/ReorderList.java
+++ src/JAVA/MEDIUM/LinkedList/ReorderList.java
@@ -14,5 +14,30 @@ public class ReorderList {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
+    class Solution {
+        public void reorderList(ListNode head) {
+            if(head==null||head.next==null) return;
+            ListNode slow=head,fast=head;
+            while(fast.next!=null && fast.next.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            ListNode rev=reverseList(slow.next);
+            slow.next=null;
 
+            ListNode st=head;
+
+            while(rev!=null){
+                ListNode temp1=st.next;
+                ListNode temp2=rev.next;
+                st.next=rev;
+                rev.next=temp1;
+
+                st=temp1;
+                rev=temp2;
+
+            }
+        }
+
+    }
 }
