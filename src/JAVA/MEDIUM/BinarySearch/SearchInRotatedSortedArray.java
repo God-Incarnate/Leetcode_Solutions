443362218a5e46e11a2b1fbcbefb6ed89547a6aa
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:59:03 +0530

    search in rotated sorted array

--- src/JAVA/MEDIUM/BinarySearch/SearchInRotatedSortedArray.java
+++ src/JAVA/MEDIUM/BinarySearch/SearchInRotatedSortedArray.java
@@ -14,6 +14,23 @@ public class SearchInRotatedSortedArray {
                 return binarySearch(nums,left,right,target);
             }
         }
-
+        private int returnIndexOfLeast(int[] arr){
+            int left=0,right=arr.length-1,mid=0;
+            while(left<right){
+                mid=left+(right-left)/2;
+                if(arr[mid]>arr[right]) left=mid+1;
+                else right=mid;
+            }
+            return left;
+        }
+        private int binarySearch(int[] arr,int left,int right,int target){
+            while(left<=right){
+                int mid=left+(right-left)/2;
+                if(target==arr[mid]) return mid;
+                else if(target<arr[mid]) right=mid-1;
+                else left=mid+1;
+            }
+            return -1;
+        }
     }
 }
