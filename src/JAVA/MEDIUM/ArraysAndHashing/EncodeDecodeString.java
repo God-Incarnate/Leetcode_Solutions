fe152075163d07367a5dca5ea0e27ef3ddbb5e4a
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:43:20 +0530

    Encode decode string

--- src/JAVA/MEDIUM/ArraysAndHashing/EncodeDecodeString.java
+++ src/JAVA/MEDIUM/ArraysAndHashing/EncodeDecodeString.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class EncodeDecodeString {
     //https://neetcode.io/problems/string-encode-and-decode/question?list=neetcode150
+    class Solution {
+        //dummy_input = ["Hello","World"]
+        public String encode(List<String> strs) {
+            StringBuilder sb = new StringBuilder();
 
+            for (String str : strs) {
+                sb.append(str.length()).append("#").append(str);
+            }
+
+            return sb.toString();
+        }
+
+    }
 
 }
