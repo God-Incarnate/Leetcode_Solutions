e2132b8fd81c29b44f27aaf90101fdcb40d782e2
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:36:42 +0530

    MaxDepthOfBT

--- src/JAVA/EASY/Trees/MaxDepthBinaryTree.java
+++ src/JAVA/EASY/Trees/MaxDepthBinaryTree.java
@@ -16,5 +16,11 @@ public class MaxDepthBinaryTree {
           }
       }
 
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root==null) return 0;
+            return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
 
+        }
+    }
 }
