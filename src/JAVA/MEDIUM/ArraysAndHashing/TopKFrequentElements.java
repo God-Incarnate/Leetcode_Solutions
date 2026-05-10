45b9963f181ea954807a4a81ed535f3eb0f84338
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:51:44 +0530

    top ke freq. eleme

--- src/JAVA/MEDIUM/ArraysAndHashing/TopKFrequentElements.java
+++ src/JAVA/MEDIUM/ArraysAndHashing/TopKFrequentElements.java
@@ -7,5 +7,41 @@ import java.util.Map;
 
 public class TopKFrequentElements {
     //https://leetcode.com/problems/top-k-frequent-elements
+    class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+            // Step 1: Frequency map
+            Map<Integer, Integer> freqMap = new HashMap<>();
+            for (int num : nums) {
+                freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+            }
 
+            // Step 2: Bucket array
+            List<Integer>[] bucket = new ArrayList[nums.length + 1];
+
+            for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+                int num = entry.getKey();
+                int freq = entry.getValue();
+
+                if (bucket[freq] == null) {
+                    bucket[freq] = new ArrayList<>();
+                }
+                bucket[freq].add(num);
+            }
+
+            // Step 3: Collect top k
+            int[] res = new int[k];
+            int idx = 0;
+
+            for (int i = bucket.length - 1; i >= 0 && idx < k; i--) {
+                if (bucket[i] != null) {
+                    for (int num : bucket[i]) {
+                        res[idx++] = num;
+                        if (idx == k) return res;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
 }
