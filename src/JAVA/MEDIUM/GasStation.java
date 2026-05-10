e0bc1a79ded340a8f013904c155d909410cb2357
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:43:14 +0530

    gas station

--- src/JAVA/MEDIUM/GasStation.java
+++ src/JAVA/MEDIUM/GasStation.java
@@ -2,5 +2,24 @@ package JAVA.MEDIUM;
 
 public class GasStation {
     //https://leetcode.com/problems/gas-station
+    class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int totalTank = 0;
+            int currTank = 0;
+            int start = 0;
 
+            for (int i = 0; i < gas.length; i++) {
+                int gain = gas[i] - cost[i];
+
+                totalTank += gain;
+                currTank += gain;
+                if (currTank < 0) {
+                    start = i + 1;
+                    currTank = 0;
+                }
+            }
+
+            return totalTank >= 0 ? start : -1;
+        }
+    }
 }
