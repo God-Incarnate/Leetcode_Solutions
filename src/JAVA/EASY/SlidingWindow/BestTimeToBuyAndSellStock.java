efbb9e7a1089648c8b59aef420b6f0ee8e1a012b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:29:49 +0530

    BestTimeToBuyandSellStocks

--- src/JAVA/EASY/SlidingWindow/BestTimeToBuyAndSellStock.java
+++ src/JAVA/EASY/SlidingWindow/BestTimeToBuyAndSellStock.java
@@ -3,5 +3,18 @@ package JAVA.EASY.SlidingWindow;
 public class BestTimeToBuyAndSellStock {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
-
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int minPrice=Integer.MAX_VALUE;
+            int maxProfit=0;
+            for (int n: prices){
+                if(n<minPrice){
+                    minPrice=n;
+                } else{
+                    maxProfit=Math.max(maxProfit,n-minPrice);
+                }
+            }
+            return maxProfit;
+        }
+    }
 }
