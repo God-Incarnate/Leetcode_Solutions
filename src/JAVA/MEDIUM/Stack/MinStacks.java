0f0c1f749ecbcedf5d5e05a05e214b22ff24d146
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:18:31 +0530

    Min stack

--- src/JAVA/MEDIUM/Stack/MinStacks.java
+++ src/JAVA/MEDIUM/Stack/MinStacks.java
@@ -4,7 +4,33 @@ import java.util.Stack;
 
 public class MinStacks {
     //https://leetcode.com/problems/min-stack
+    class MinStack {
+        Stack<Integer> stack;
+        Stack<Integer> minStack;
 
+        public MinStack() {
+            this.stack=new Stack<>();
+            this.minStack=new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            if(minStack.isEmpty()||(val<=minStack.peek())) minStack.push(val);
+        }
+
+        public void pop() {
+            int val=stack.pop();
+            if(val==minStack.peek()) minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
 
 /**
  * Your MinStack object will be instantiated and called as such:
