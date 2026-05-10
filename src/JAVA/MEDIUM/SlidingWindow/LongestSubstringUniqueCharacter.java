fb3091d4e46cae33201b37f64364cfe67dd0315e
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:11:09 +0530

    Logest substring unique character

--- src/JAVA/MEDIUM/SlidingWindow/LongestSubstringUniqueCharacter.java
+++ src/JAVA/MEDIUM/SlidingWindow/LongestSubstringUniqueCharacter.java
@@ -4,5 +4,21 @@ import java.util.HashSet;
 
 public class LongestSubstringUniqueCharacter {
     //https://leetcode.com/problems/longest-substring-without-repeating-characters
-
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            HashSet<Character> ptr=new HashSet<>();
+            int left=0;
+            int max=0;
+            for(int right=0;right<s.length();right++){
+                char c=s.charAt(right);
+                while(ptr.contains(c)){
+                    ptr.remove(s.charAt(left));
+                    left++;
+                }
+                ptr.add(c);
+                max=Math.max(max,right-left+1);
+            }
+            return max;
+        }
+    }
 }
