272a90bd54dbce90888fc2766d2a52d5d18e8d07
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:01:04 +0530

    duplicates

--- src/JAVA/EASY/ArraysAndHashing/ContainsDuplicates.java
+++ src/JAVA/EASY/ArraysAndHashing/ContainsDuplicates.java
@@ -7,5 +7,18 @@ import java.util.Set;
 public class ContainsDuplicates {
     //https://leetcode.com/problems/contains-duplicate
 
+    //Using Sorting:
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
 }
