02111198d5fddde6c0435aefc70aac4c55e147f8
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:18:05 +0530

    Min stack

---
+++ src/JAVA/MEDIUM/Stack/MinStacks.java
@@ -0,0 +1,17 @@
+package JAVA.MEDIUM.Stack;
+
+import java.util.Stack;
+
+public class MinStacks {
+    //https://leetcode.com/problems/min-stack
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+}
