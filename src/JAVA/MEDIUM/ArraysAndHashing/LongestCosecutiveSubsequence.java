2421f5fe92b4117e0e92a254561809f8a44f9de7
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:49:36 +0530

    longest consecutive subsequence

--- src/JAVA/MEDIUM/ArraysAndHashing/LongestCosecutiveSubsequence.java
+++ src/JAVA/MEDIUM/ArraysAndHashing/LongestCosecutiveSubsequence.java
@@ -4,5 +4,27 @@ import java.util.HashSet;
 
 public class LongestCosecutiveSubsequence {
     //https://leetcode.com/problems/longest-consecutive-sequence
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            if(nums.length==0) return 0;
+            if(nums.length==1) return 1;
+            HashSet<Integer> numsVal=new HashSet<>();
+            for(int num:nums){
+                numsVal.add(num);
+            }
+            int maxC=0;
+            for(int num:numsVal){
+                if(!numsVal.contains(num-1)){
+                    int countC=0;
+                    while(numsVal.contains(num+1)){
+                        countC++;
+                        num++;
+                    }
+                    maxC=Math.max(maxC,countC);
+                }
 
+            }
+            return maxC+1;
+        }
+    }
 }
