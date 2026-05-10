ad50c62f4411f3080ef3b45240d5e6c10273dec8
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:26:32 +0530

    lowest common ancestor

--- src/JAVA/MEDIUM/Trees/LowestCommonAncestorBST.java
+++ src/JAVA/MEDIUM/Trees/LowestCommonAncestorBST.java
@@ -3,5 +3,13 @@ package JAVA.MEDIUM.Trees;
 public class LowestCommonAncestorBST {
     //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+
+
 
 }
