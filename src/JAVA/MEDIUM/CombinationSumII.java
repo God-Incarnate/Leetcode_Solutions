bdb5bf7436d9cd6e0786c5c5d3734fe7ce303981
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:38:37 +0530

    combination sum ii

--- src/JAVA/MEDIUM/CombinationSumII.java
+++ src/JAVA/MEDIUM/CombinationSumII.java
@@ -6,5 +6,13 @@ import java.util.List;
 
 public class CombinationSumII {
     //https://leetcode.com/problems/combination-sum-ii
+    class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result=new ArrayList<>();
+            Arrays.sort(candidates);
+            backtrack(0,target,new ArrayList<>(),candidates,result);
+            return result;
+        }
 
+    }
 }
