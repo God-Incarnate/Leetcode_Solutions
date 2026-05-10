dab0983cd28d3a0542e33f6e51b44beba845e985
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:00:56 +0530

    time based key value store

--- src/JAVA/MEDIUM/BinarySearch/TimeBasedKeyValueStore.java
+++ src/JAVA/MEDIUM/BinarySearch/TimeBasedKeyValueStore.java
@@ -49,7 +49,29 @@ public class TimeBasedKeyValueStore {
             }
         }
     }
+    /*
+    class TimeMap {
+    Map<String,TreeMap<Integer,String>> map;
 
+    public TimeMap() {
+        this.map=new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key)) return "";
+
+        TreeMap<Integer, String> tm = map.get(key);
+        Integer t = tm.floorKey(timestamp);
+
+        return t == null ? "" : tm.get(t);
+    }
+}
+     */
 
 /**
  * Your TimeMap object will be instantiated and called as such:
