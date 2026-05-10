f73011c84326dc26a18ed10492564d20782b8922
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:54:26 +0530

    binary search in 2d matrix

--- src/JAVA/MEDIUM/BinarySearch/BinarySearch2DMatrix.java
+++ src/JAVA/MEDIUM/BinarySearch/BinarySearch2DMatrix.java
@@ -2,5 +2,24 @@ package JAVA.MEDIUM.BinarySearch;
 
 public class BinarySearch2DMatrix {
     //https://leetcode.com/problems/search-a-2d-matrix
+    class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int m=matrix.length,n=matrix[0].length;
+            int left=0;
+            int right=m*n-1;
+            int mid=0;
 
+            while(left<=right){
+                mid=left+(right-left)/2;
+                int row=mid/n;
+                int col=mid%n;
+                int val=matrix[row][col];
+                if(val==target) return true;
+                else if (val<target) left=mid+1;
+                else right=mid-1;
+            }
+            return false;
+        }
+
+    }
 }
