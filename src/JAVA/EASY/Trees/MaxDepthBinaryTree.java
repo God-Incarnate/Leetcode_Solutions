e33a3fcecfc8063288b9dcd3f52aa884d849bd2d
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:36:26 +0530

    MaxDepthOfBT

--- src/JAVA/EASY/Trees/MaxDepthBinaryTree.java
+++ src/JAVA/EASY/Trees/MaxDepthBinaryTree.java
@@ -3,5 +3,18 @@ package JAVA.EASY.Trees;
 public class MaxDepthBinaryTree {
     //https://leetcode.com/problems/maximum-depth-of-binary-tree
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
 }
