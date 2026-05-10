b2b776290549754cd09777e0c8dce146b4ce6ef4
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:55:56 +0530

    koko banana

--- src/JAVA/MEDIUM/BinarySearch/KokosBanana.java
+++ src/JAVA/MEDIUM/BinarySearch/KokosBanana.java
@@ -2,5 +2,26 @@ package JAVA.MEDIUM.BinarySearch;
 
 public class KokosBanana {
     //https://leetcode.com/problems/koko-eating-bananas
+    class Solution {
+        public int minEatingSpeed(int[] piles, int h) {
+            int left = 1;
+            int right = getMax(piles);
+            int answer = right;
 
+            while (left <= right) {
+                int midSpeed = left + (right - left) / 2;
+
+                if (canFinish(piles, h, midSpeed)) {
+                    answer = midSpeed;
+                    right = midSpeed - 1;
+                } else {
+                    left = midSpeed + 1;
+                }
+            }
+
+            return answer;
+        }
+
+
+    }
 }
