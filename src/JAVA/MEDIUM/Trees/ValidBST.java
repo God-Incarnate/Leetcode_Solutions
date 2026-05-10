eeddd3f150ff93964a62334373225b870ca5bfc3
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:28:20 +0530

    valid bst

--- src/JAVA/MEDIUM/Trees/ValidBST.java
+++ src/JAVA/MEDIUM/Trees/ValidBST.java
@@ -3,6 +3,20 @@ package JAVA.MEDIUM.Trees;
 public class ValidBST {
     //https://leetcode.com/problems/validate-binary-search-tree
     
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+
 
 
 }
