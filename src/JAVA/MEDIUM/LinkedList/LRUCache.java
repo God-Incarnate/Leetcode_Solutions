b6b33b3001b0eab7a310e00c403d714cf143d47b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:04:43 +0530

    LRU cache

---
+++ src/JAVA/MEDIUM/LinkedList/LRUCache.java
@@ -0,0 +1,16 @@
+package JAVA.MEDIUM.LinkedList;
+
+import java.util.HashMap;
+
+public class LRUCache {
+    //https://leetcode.com/problems/lru-cache
+
+
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
