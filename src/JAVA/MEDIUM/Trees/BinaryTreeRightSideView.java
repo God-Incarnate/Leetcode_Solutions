1d922ceda431c516d9d00a5083f4a2f6218796a6
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:19:59 +0530

    BinaryTree right side view

--- src/JAVA/MEDIUM/Trees/BinaryTreeRightSideView.java
+++ src/JAVA/MEDIUM/Trees/BinaryTreeRightSideView.java
@@ -8,5 +8,18 @@ import java.util.Queue;
 public class BinaryTreeRightSideView {
     //https://leetcode.com/problems/binary-tree-right-side-view
 
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
 }
