b832c99dec5636f8750a20e3a1e38fe88ad7595b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:24:09 +0530

    level order traversal

---
+++ src/JAVA/MEDIUM/Trees/LevelOrderTraversal.java
@@ -0,0 +1,12 @@
+package JAVA.MEDIUM.Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    //https://leetcode.com/problems/binary-tree-level-order-traversal
+
+
+}
