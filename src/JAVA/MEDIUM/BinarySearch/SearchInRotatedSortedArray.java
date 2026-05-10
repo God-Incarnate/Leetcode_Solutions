299e1a6a70412c130562abe9854d625a69f1fb79
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:58:39 +0530

    search in rotated sorted array

--- src/JAVA/MEDIUM/BinarySearch/SearchInRotatedSortedArray.java
+++ src/JAVA/MEDIUM/BinarySearch/SearchInRotatedSortedArray.java
@@ -2,5 +2,18 @@ package JAVA.MEDIUM.BinarySearch;
 
 public class SearchInRotatedSortedArray {
     //https://leetcode.com/problems/search-in-rotated-sorted-array
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left=0,pivot=returnIndexOfLeast(nums),right=nums.length-1;
+            if(target>nums[right] && target<nums[left]) return -1;
+            if(pivot==left){
+                return binarySearch(nums,left,right,target);
+            }else{
+                if(target>nums[right]) right=pivot-1;
+                else left=pivot;
+                return binarySearch(nums,left,right,target);
+            }
+        }
 
+    }
 }
