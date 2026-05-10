08ff87cf7313955ab027bc7772ab1d00878450f3
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:49:17 +0530

    longest consecutive subsequence

---
+++ src/JAVA/MEDIUM/ArraysAndHashing/LongestCosecutiveSubsequence.java
@@ -0,0 +1,8 @@
+package JAVA.MEDIUM.ArraysAndHashing;
+
+import java.util.HashSet;
+
+public class LongestCosecutiveSubsequence {
+    //https://leetcode.com/problems/longest-consecutive-sequence
+
+}
