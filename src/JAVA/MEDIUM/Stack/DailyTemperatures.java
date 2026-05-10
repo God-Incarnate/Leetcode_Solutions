04287d6b6fd26c6c60608c08a56fc5724cbbf191
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:15:55 +0530

    daily temp.

--- src/JAVA/MEDIUM/Stack/DailyTemperatures.java
+++ src/JAVA/MEDIUM/Stack/DailyTemperatures.java
@@ -4,5 +4,18 @@ import java.util.Stack;
 
 public class DailyTemperatures {
     //https://leetcode.com/problems/daily-temperatures
-
+    class Solution {
+        public int[] dailyTemperatures(int[] temperatures) {
+            int[] res=new int[temperatures.length];
+            Stack<Integer> stack=new Stack<>();
+            for(int i=0;i<temperatures.length;i++){
+                while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                    int prevIndex=stack.pop();
+                    res[prevIndex]=i-prevIndex;
+                }
+                stack.push(i);
+            }
+            return res;
+        }
+    }
 }
