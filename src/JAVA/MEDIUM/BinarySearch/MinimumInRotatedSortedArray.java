a7b1412430305a702398791fa27465eaf4e16aed
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:57:27 +0530

    minimum in rotated sorted array

--- src/JAVA/MEDIUM/BinarySearch/MinimumInRotatedSortedArray.java
+++ src/JAVA/MEDIUM/BinarySearch/MinimumInRotatedSortedArray.java
@@ -2,5 +2,15 @@ package JAVA.MEDIUM.BinarySearch;
 
 public class MinimumInRotatedSortedArray {
     //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
-
+    class Solution {
+        public int findMin(int[] nums) {
+            int left=0,right=nums.length-1,mid=0;
+            while(left<right){
+                mid=left+(right-left)/2;
+                if(nums[mid]>nums[right]) left=mid+1;
+                else right=mid;
+            }
+            return nums[left];
+        }
+    }
 }
