60c29fe40b0b0e2a41d0528aeb8446f1aa1df8e8
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 18:28:17 +0530

    MinCostClimbingStairs

--- src/JAVA/EASY/OneDDP/MinCostClimbingStairs.java
+++ src/JAVA/EASY/OneDDP/MinCostClimbingStairs.java
@@ -2,5 +2,16 @@ package JAVA.EASY.OneDDP;
 
 public class MinCostClimbingStairs {
     //https://leetcode.com/problems/min-cost-climbing-stairs
-
+    class Solution {
+        public int minCostClimbingStairs(int[] cost) {
+            int value0=cost[0];
+            int value1=cost[1];
+            for(int i=2;i<cost.length;i++){
+                int costSoFar=cost[i]+Math.min(value0,value1);
+                value0=value1;
+                value1=costSoFar;
+            }
+            return Math.min(value0,value1);
+        }
+    }
 }
