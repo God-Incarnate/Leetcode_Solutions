cd3d03b285a9ca08978abad1a4c2e0f137ea6f81
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:39:59 +0530

    design twiter

---
+++ src/JAVA/MEDIUM/DesignTwitter.java
@@ -0,0 +1,17 @@
+package JAVA.MEDIUM;
+
+import java.util.*;
+
+public class DesignTwitter {
+    //https://leetcode.com/problems/design-twitter
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
+}
