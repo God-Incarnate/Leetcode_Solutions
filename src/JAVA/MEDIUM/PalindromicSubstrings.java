456bc061cefd2759eb72e1cf6b6df6e08f3f0dba
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 20:00:57 +0530

    palindromic substring

--- src/JAVA/MEDIUM/PalindromicSubstrings.java
+++ src/JAVA/MEDIUM/PalindromicSubstrings.java
@@ -2,5 +2,23 @@ package JAVA.MEDIUM;
 
 public class PalindromicSubstrings {
     //https://leetcode.com/problems/palindromic-substrings
-
+    class Solution {
+        public int countSubstrings(String s) {
+            int count=0;
+            for(int i=0;i<s.length();i++){
+                count+=expand(s,i,i);
+                count+=expand(s,i,i+1);
+            }
+            return count;
+        }
+        private int expand(String s, int l, int r){
+            int count=0;
+            while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                count++;
+                l--;
+                r++;
+            }
+            return count;
+        }
+    }
 }
