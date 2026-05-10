ea850126863a3710021d5298f365553ac838183b
God-Incarnate <prashant11111999@gmail.com>
2026-05-10 19:35:42 +0530

    2sum 2

--- src/JAVA/MEDIUM/TwoPointers/TwoSumIISortedArray.java
+++ src/JAVA/MEDIUM/TwoPointers/TwoSumIISortedArray.java
@@ -2,5 +2,21 @@ package JAVA.MEDIUM.TwoPointers;
 
 public class TwoSumIISortedArray {
     //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
-
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int[] res=new int[2];
+            int pt2=numbers.length-1;
+            int pt1=0;
+            while(pt1<pt2){
+                if((numbers[pt1]+numbers[pt2])<target) pt1++;
+                else if((numbers[pt1]+numbers[pt2])>target) pt2--;
+                else {
+                    res[0]=pt1+1;
+                    res[1]=pt2+1;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
 }
